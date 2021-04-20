4851a0ad0f221f851e5de7955ec47cc6f050bab8
AceIsHuman <menoman1995@yahoo.com>
2021-04-20 02:26:54 -0500

    create NameSection with full name and QR code

---
+++ src/components/NameSection.js
@@ -0,0 +1,12 @@
+import nameQR from '../assets/nameQR.svg';
+
+function NameSection() {
+  return (
+    <section>
+      <h2>Aciel Ochoa</h2>
+      <img src={nameQR} width="100" alt="qr-code-aciel-ochoa"/>
+    </section>
+  )
+}
+
+export default NameSection;
