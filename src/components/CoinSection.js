d6fdbffe0b046f00dbbd16021cd21c7a8b134a36
AceIsHuman <menoman1995@yahoo.com>
2021-04-20 13:58:37 -0500

    create HTTP request for coin information and pass to CoinCard components

--- src/components/CoinSection.js
+++ src/components/CoinSection.js
@@ -1,17 +1,39 @@
+import { useState, useEffect } from 'react';
+import cmcRequest from '../utils/cmcAPI/cmcRequest';
 import CoinCard from './CoinCard';
 
 function CryptoCurrencySection() {
-  const coin = [
-    { name: 'Tezos (XTZ)', uuid: 'fsIbGOEJWbzxG' },
-    { name: 'BurstCoin (BURST)', uuid: 'LUbkfWw7GwJ73' },
-    { name: 'Cardano (ADA)', uuid: 'qzawljRxB5bYu' },
-  ];
+  const [coin, setCoin] = useState([]);
+
+  useEffect(() => {
+    // CoinMarketCap ids for [Tezos, Burst, Cardano]
+    const ids = [2011, 573, 2010];
+
+    // Make CMC API request to retrive prices
+    cmcRequest()
+      .get(`/cryptocurrency/quotes/latest?id=${ids.join(',')}`)
+      .then((res) => {
+        let data = [];
+        for (let coin of res.data) {
+          data.push({
+            id: coin.id,
+            name: coin.name,
+            symbol: coin.symbol,
+            price: `$${coin.quote.USD.price.toFixed(5)}`,
+          });
+        }
+        // Store prices from retrieved data in state.
+        setCoin(data);
+      })
+      .catch((err) => console.error(err.message));
+  });
 
   return (
     <div>
-      {coin.map((c) => {
-        return <CoinCard name={c.name} uuid={c.uuid} />;
-      })}
+      {coin.length &&
+        coin.map((c) => {
+          return <CoinCard key={`coin-card-${c.id}`} coin={c} />;
+        })}
     </div>
   );
 }
