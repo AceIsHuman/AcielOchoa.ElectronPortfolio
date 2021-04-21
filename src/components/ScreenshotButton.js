12d2e230e8c4102d951dd517905f3830158c8e31
AceIsHuman <menoman1995@yahoo.com>
2021-04-21 14:51:54 -0500

    style screenshot button

--- src/components/ScreenshotButton.js
+++ src/components/ScreenshotButton.js
@@ -1,5 +1,29 @@
+import styled from 'styled-components';
+import palette from '../assets/colorScheme';
+
 function ScreenshotButton() {
-  return <button>Take Screenshot</button>;
+  return (
+    <Button>
+      Take
+      <br />
+      Screenshot
+    </Button>
+  );
 }
+
+const Button = styled.button`
+  position: fixed;
+  top: 1%;
+  right: 1%;
+  background-color: ${palette.accentPrimary};
+  color: ${palette.fontSecondary};
+  opacity: 0.7;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  transition: 0.2s;
+  &:hover {
+    opacity: 1;
+  }
+`
 
 export default ScreenshotButton;
