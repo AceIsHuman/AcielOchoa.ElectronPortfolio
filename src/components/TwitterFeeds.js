8f8abe93d9c787ab0e0e6c510387a52d6c4c73e0
AceIsHuman <menoman1995@yahoo.com>
2021-04-21 14:27:51 -0500

    use display: grid to style feed positions

--- src/components/TwitterFeeds.js
+++ src/components/TwitterFeeds.js
@@ -1,12 +1,22 @@
 import TwitterTimeline from './TwitterTimeline';
 
+// Styling imports
+import styled from 'styled-components';
+
 function TwitterFeeds() {
   return (
-    <section>
+    <Wrapper>
       <TwitterTimeline screenName="BBCAfrica" />
       <TwitterTimeline screenName="Ethereum" />
-    </section>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.section`
+  margin-top: 2rem;
+  width: 95%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
 
 export default TwitterFeeds;
