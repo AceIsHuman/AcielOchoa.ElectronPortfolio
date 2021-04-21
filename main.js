090e0e5837c1c7a41c5de4ab399a501de645aa38
AceIsHuman <menoman1995@yahoo.com>
2021-04-21 01:35:10 -0500

    update loadURL path and mainWindow webPreferences

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, './.env') })
-
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+
+const isMacOS = process.platform === 'darwin';
+const isDev = !app.isPackaged;
 
 let mainWindow;
-const isMacOS = process.platform === 'darwin';
 
 function createWindow() {
   // Create browser window
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      width: 1200,
+      height: 800,
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+    },
+  });
 
   // Set load URL
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
-      pathname: path.join(__dirname, '/../build/index.html'),
+      pathname: path.join(__dirname, './build/index.html'),
       protocol: 'file:',
       slashes: true,
     });
   mainWindow.loadURL(startUrl);
 
   // Open dev tools is environment is not production
-  if (process.environment !== 'production')
-    mainWindow.webContents.openDevTools();
+  if (isDev) mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', createWindow);
