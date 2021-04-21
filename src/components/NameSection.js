201ec2df4be261f1cf0a5b27f4ad020e1c7461cc
AceIsHuman <menoman1995@yahoo.com>
2021-04-21 13:56:39 -0500

    style name section

--- src/components/NameSection.js
+++ src/components/NameSection.js
@@ -1,12 +1,20 @@
 import QRCode from './QRCode';
+import styled from 'styled-components';
+import palette from '../assets/colorScheme';
 
 function NameSection() {
   return (
-    <section>
-      <h2>Aciel Ochoa</h2>
+    <section style={{ paddingTop: '1rem' }}>
+      <Name>Aciel Ochoa</Name>
       <QRCode id="full-name" data="Aciel Ochoa" />
     </section>
-  )
+  );
 }
+
+const Name = styled.h2`
+  color: ${palette.fontSecondary};
+  text-decoration: underline;
+  text-align: center;
+`;
 
 export default NameSection;
