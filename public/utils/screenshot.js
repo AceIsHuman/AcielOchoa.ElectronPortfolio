ef4b8be4cabd1e57b77abd4bf50e0ddaa71eb633
AceIsHuman <menoman1995@yahoo.com>
2021-04-21 18:03:59 -0500

    create script that prompts user to choose location to save a screenshot

---
+++ public/utils/screenshot.js
@@ -0,0 +1,33 @@
+const { BrowserWindow, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+function screenshot() {
+  const win = BrowserWindow.getFocusedWindow();
+  win.webContents.capturePage({ x: 0, y: 0 }).then((img) => {
+    dialog
+      .showSaveDialog({
+        title: 'Select file path to save screenshot',
+        defaultPath: path.join(__dirname, '../assets/image.png'),
+        buttonLabel: 'Save',
+        filters: [{ name: 'Image Files', extensions: ['png'] }],
+        properties: [],
+      })
+      .then((file) => {
+        if (!file.canceled) {
+          fs.writeFile(
+            file.filePath.toString(),
+            img.toPNG(),
+            'base64',
+            (err) => {
+              if (err) throw err;
+              console.log('Saved!');
+            }
+          );
+        }
+      })
+      .catch((err) => console.error(err));
+  });
+}
+
+module.exports = screenshot;
