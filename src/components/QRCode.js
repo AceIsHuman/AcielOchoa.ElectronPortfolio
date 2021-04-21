aaae223c54084ea0cf7db5505ac65e7fe8bc6d54
AceIsHuman <menoman1995@yahoo.com>
2021-04-21 02:46:09 -0500

    create component to render QRCodes

---
+++ src/components/QRCode.js
@@ -0,0 +1,28 @@
+import QRCode from 'qrcode';
+import { useEffect, useRef } from 'react';
+
+function QRCodeComponent(props) {
+  const { data, id } = props;
+  const qrRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = document.getElementById(`qr-code-${data}`);
+    createCode(data, qrRef.current);
+  }, [data])
+
+  return (
+    <canvas id={`qr-code-${id}`} ref={qrRef}></canvas>
+  )
+}
+
+function createCode(data, canvas) {
+  const options = {
+    width: 100,
+    height: 100
+  }
+  QRCode.toCanvas(canvas, data, options, err => {
+    if (err) console.error(err);
+  });
+};
+
+export default QRCodeComponent;
