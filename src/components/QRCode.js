0d0927cdd4d498c2fdff09a966372ac7894d13a7
AceIsHuman <menoman1995@yahoo.com>
2021-04-21 20:23:53 -0500

    allow size to be dynamically controlled with size prop

--- src/components/QRCode.js
+++ src/components/QRCode.js
@@ -2,26 +2,31 @@ import QRCode from 'qrcode';
 import { useEffect, useRef } from 'react';
 
 function QRCodeComponent(props) {
-  const { data, id } = props;
+  const { data, id, size } = props;
   const qrRef = useRef(null);
 
   useEffect(() => {
-    createCode(data, qrRef.current);
-  }, [data])
+    createCode(data, qrRef.current, { size });
+  }, [data, size]);
 
   return (
-    <canvas id={`qr-code-${id}`} ref={qrRef}></canvas>
-  )
+    <canvas
+      id={`qr-code-${id}`}
+      ref={qrRef}
+      style={{ borderRadius: '1rem' }}
+    ></canvas>
+  );
 }
 
-function createCode(data, canvas) {
+
+function createCode(data, canvas, properties) {
   const options = {
-    width: 100,
-    height: 100
-  }
-  QRCode.toCanvas(canvas, data, options, err => {
+    width: properties.size || 100,
+    height: properties.size || 100,
+  };
+  QRCode.toCanvas(canvas, data, options, (err) => {
     if (err) console.error(err);
   });
-};
+}
 
 export default QRCodeComponent;
