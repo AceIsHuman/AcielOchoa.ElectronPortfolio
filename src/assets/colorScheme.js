0e7a8321c37594dea9707ea2ca4a73b79b11ea49
AceIsHuman <menoman1995@yahoo.com>
2021-04-21 23:55:14 -0500

    add accentSecondary value

--- src/assets/colorScheme.js
+++ src/assets/colorScheme.js
@@ -3,7 +3,8 @@ const palette = {
   secondary: '#212527',
   fontPrimary: '#212527',
   fontSecondary: '#F6F6F6',
-  accentPrimary: '#4B6DF5'
-}
+  accentPrimary: '#4B6DF5',
+  accentSecondary: '#5d8b3d',
+};
 
 export default palette;
