6d35feffb03c310a899fef49fa4964c6c7313b65
AceIsHuman <menoman1995@yahoo.com>
2021-04-22 01:28:22 -0500

    overlay badge on QRCode

--- src/components/NameSection.js
+++ src/components/NameSection.js
@@ -1,21 +1,38 @@
 import QRCode from './QRCode';
+import logo from '../assets/logo.svg';
 import styled from 'styled-components';
 import palette from '../assets/colorScheme';
 
 function NameSection() {
   return (
-    <section style={{ paddingTop: '1rem' }}>
+    <div>
       <Name>Aciel Ochoa</Name>
-      <QRCode id="full-name" data="Aciel Ochoa" />
-    </section>
+      <QRWrapper>
+        <QRCode id="full-name" data="Aciel Ochoa" />
+        <Badge src={logo} alt="aciel-ochoa-logo" />
+      </QRWrapper>
+    </div>
   );
 }
+
+const QRWrapper = styled.div`
+  position: relative;
+`;
 
 const Name = styled.h2`
   color: ${palette.fontSecondary};
   text-decoration: underline;
   text-align: center;
   margin-bottom: 0.5rem;
+`;
+
+const Badge = styled.img`
+  width: 2.5rem;
+  height: 2.5rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 export default NameSection;
