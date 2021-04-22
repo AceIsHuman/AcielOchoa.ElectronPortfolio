ca401138244f0706d721b220faeb4c8bb24493e0
AceIsHuman <menoman1995@yahoo.com>
2021-04-22 00:51:16 -0500

    use width/height from props to render timeline

--- src/components/TwitterTimeline.js
+++ src/components/TwitterTimeline.js
@@ -1,13 +1,16 @@
 import { Timeline } from 'react-twitter-widgets';
 
 function TwitterTimeline(props) {
-  const { screenName } = props;
+  const { screenName, width, height } = props;
+
   return (
     <div>
-      <Timeline
-        dataSource={{ sourceType: 'profile', screenName }}
-        options={{ width: 500, height: 600 }}
-      />
+      {width && height && (
+        <Timeline
+          dataSource={{ sourceType: 'profile', screenName }}
+          options={{ width, height }}
+        />
+      )}
     </div>
   );
 }
